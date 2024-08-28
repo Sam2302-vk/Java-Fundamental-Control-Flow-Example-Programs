e80a0a9a42b7fc8418debb1a2b42c346a4aaa253
Sam2302-vk <samvenkateshkumar@gmail.com>
2024-08-28 12:32:06 +0530

    Add files via upload

---
+++ Block.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Block {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int i=1;
+        do{
+            System.out.println(i);
+            i++ ;
+        }
+        while(i<=num);
+
+    }
+}
---
+++ Week.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Week {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int weekNum=sc.nextInt();
+        String weekDay;
+        switch(weekNum){
+            case 1:
+               weekDay="Monday";
+            break;
+
+            case 2:
+              weekDay="Tuesday";
+              break;
+
+              case 3:
+              weekDay="Wednesday";
+              break;
+
+              case 4:
+              weekDay="Thursaday";
+              break;
+
+              case 5:
+              weekDay="Friday";
+              break;
+
+              case 6:
+              weekDay="Saturday";
+              break;
+
+              case 7:
+              weekDay="Sunday";
+              break;
+
+
+              default:
+              weekDay="Invalid Week day";
+
+            
+        }
+        System.out.println(weekDay);
+    }
+}
