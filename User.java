e292d3d02559c1dc739de3e389c5bc3eb48ff6b4
Sam2302-vk <samvenkateshkumar@gmail.com>
2024-08-28 12:36:53 +0530

    Update User.java

--- User.java
+++ User.java
@@ -1,5 +1,6 @@
-import java.util.*;
+//Print the numbers from 1 to the user's number
 
+import java.util.*;
 public class User {
     public static void main(String[] args) {
         Scanner sc=new Scanner (System.in);
